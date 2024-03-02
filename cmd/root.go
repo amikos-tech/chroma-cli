dbecde23cdd3a3c5628594accab88d49b97dc5a1
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-02 20:16:54 +0200

    feat: Initial chroma config

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -28,13 +33,44 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.chroma-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	cobra.OnInitialize(initConfig)
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	fmt.Println("Initializing config")
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+
+	}
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(home + "/.chroma")
+
+	if err := viper.ReadInConfig(); err != nil {
+
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(err, &configFileNotFoundError) {
+			//create config file
+			err := os.MkdirAll(home+"/.chroma", 0700)
+			if err != nil {
+				// Unable to create directory
+				log.Fatal(err)
+			}
+			_, err = os.Create(home + "/.chroma" + "/config.yaml")
+			if err != nil {
+				//Unable to create file
+				log.Fatal(err)
+			}
+			err = viper.WriteConfig()
+			if err != nil {
+				fmt.Println("Can't initialize config:", err)
+				os.Exit(1)
+			}
+		}
+
+	}
 }
